7cea05ef8aa6889fb4ea3ecbc961e02480f1864b
sambhaji dhore <sambhajisd4@gmail.com>
2023-06-17 16:23:16 +0530

    server deployed

--- Server/Signin-Signup-Server/middleware/auth.js
+++ Server/Signin-Signup-Server/middleware/auth.js
@@ -1,19 +1,24 @@
 const jwt = require("jsonwebtoken")
 
 function auth(req,res,next){
-    const token = req.headers.authorization.split(" ")[1]
-    if(token){
-        var decoded = jwt.verify(token, 'accesstoken');
-        if(decoded){
-            req.body.userID = decoded.userID
-            next()
+    try{
+        const token = req.headers.authorization.split(" ")[1]
+        if(token){
+            var decoded = jwt.verify(token, 'accesstoken');
+            if(decoded){
+                req.body.userID = decoded.userID
+                next()
+            }else{
+                res.status(401).send({"msg":"Invalid User From Middleware auth "})
+            }
         }else{
-            res.status(401).send({"msg":"Invalid User From Middleware auth "})
+            res.status(401).send({"msg":"Please Login First "})
+    
         }
-    }else{
-        res.status(401).send({"msg":"Please Login First "})
-
+    }catch(err){
+        res.status(400).send({"msg":err});
     }
+   
 }
 
 module.exports = {auth};
